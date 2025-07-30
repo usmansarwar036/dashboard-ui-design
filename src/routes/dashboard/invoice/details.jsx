2fca281cae82d371f6b0dd2168b164d12f00dd81
Usman Sarwar <79826703+usmansarwar036@users.noreply.github.com>
2025-07-30 22:30:40 +0500

    .

--- src/routes/dashboard/invoice/details.jsx
+++ src/routes/dashboard/invoice/details.jsx
@@ -3,7 +3,6 @@ import { User, CalendarDays, CreditCard, Wallet, CheckCircle, Clock } from "luci
 // updated
 
 export default function InvoiceDetailsPage() {
-    return "hello";
     const products = [
         {
             name: "Flowbite Developer Edition",
